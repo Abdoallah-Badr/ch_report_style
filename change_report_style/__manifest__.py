a61a0ed603588de64ef96fed832359b6940fedd6
Abdoallah-Badr <abdoallahbadr@outlook.com>
2024-04-16 17:05:48 +0200

    init files

---
+++ change_report_style/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Edit company Options",
+    'author': "Upward co.",
+    'version': '17.0',
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+
+    ],
+
+    'installable': True,
+}
